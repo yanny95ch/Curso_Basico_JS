dcbbb67ecf9f8ba28e5501a94f21bd4cd5ccc0ed
yannych1995 <yannymaly@gmal.com>
2024-05-30 14:29:27 -0500

    Clases y anatomia de Variables

---
+++ clase-variables.js
@@ -0,0 +1,18 @@
+let cajaDeAndy = 'woody'
+console.log(cajaDeAndy);
+
+//Lo NO permitido: el nombre de la variable debe ser descriptivo, nada de abreviaciones, ya que dificultara en un futuro distingui dicha variable
+;
+let c = 'woody';
+let cda = 'woody';
+let pcAndy= 'woody';
+
+
+//Lo Permitido:
+let primertrasteodeAndy= 'woody';
+let urlUsuario='https://www.google.com/?hl=es';
+let idDelUsuario= '1234567890';
+
+
+// let : se utiliza cuando la variable cambia constantemente su valor, no es fijo
+//const: variables constantes o fijas
