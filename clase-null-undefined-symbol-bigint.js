9af44771f83f0e6aa2b8833999bd56d6a6d2f25a
yannych1995 <yannymaly@gmal.com>
2024-05-31 15:50:44 -0500

    Clase Boolean

---
+++ clase-null-undefined-symbol-bigint.js
@@ -0,0 +1,27 @@
+//Null
+const snoopy = null;
+console.log(snoopy);
+console.log(typeof snoopy);
+
+//Undefined
+let name 
+console.log(name);
+
+//Symbol
+const uniqueId = Symbol('id');
+const symbol1 = Symbol(1);
+const symbol2 = Symbol(1);
+console.log(symbol1 === symbol2);
+
+const ID = Symbol('id');
+let user = {}
+user[ID]='1234'
+console.log(user);
+
+//Bigint
+
+const bigNumber = 32165465136554325154n
+console.log(bigNumber);
+
+const  numberParticlesInTheUniverse = 100000000000000n 
+console.log(numberParticlesInTheUniverse);
