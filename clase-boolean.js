3771cfe06931122c6d519dc28448bf9ef3cb2a3e
yannych1995 <yannymaly@gmal.com>
2024-05-31 15:21:39 -0500

    Clase Boolean

---
+++ clase-boolean.js
@@ -0,0 +1,19 @@
+const isActive = true;
+const hasPermission = false;
+
+//Conversion Implicita
+const result = 5 > 3
+console.log(result);
+
+const name = 'Platzi';
+console.log(!!name);
+
+//Converson Explcita 
+const value = 0;
+const otherValue = 24;
+const explicitBoolean = Boolean(value);
+const explicitBoolean1 = Boolean(otherValue);
+
+console.log(explicitBoolean);
+console.log(explicitBoolean1);
+
