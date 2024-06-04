4dd45bc66713004f425343658a7f8677e99d02e4
yannych1995 <yannymaly@gmal.com>
2024-06-04 11:20:05 -0500

    Clase Bind Call y Apply

---
+++ clase-bind-call-y-apply.js
@@ -0,0 +1,29 @@
+const owner = 'Juan'
+const address = 'Avenue 1234'
+
+function dogGreeting (owner, address){
+    console.log(`Hi I'm ${this.dogName} and I live with ${owner} on ${address}`);
+}
+const newHouse = {
+    dogName: 'Azrieta'
+}
+
+dogGreeting.call(newHouse, owner, address)
+
+const necessaryValues = [owner, address]
+dogGreeting.apply(newHouse, necessaryValues)
+
+const bindinWithBind =  dogGreeting.bind(newHouse, owner, address)
+bindinWithBind()
+
+
+//Ejercicio
+
+const caricatura = { nombre:'vaca y pollito'}
+
+function recuerdo(personaje){
+    console.log(`${this.nombre} era mi caricatura favorita. Me encantaba ver las aventuras de ${personaje}`);
+}
+
+recuerdo.call(caricatura, 'vaca')
+recuerdo.call(caricatura, 'pollito')
