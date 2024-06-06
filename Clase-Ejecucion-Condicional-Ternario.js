1f8f73b05765dcaff98ca8f217f9b7e75c601f0c
yannych1995 <yannymaly@gmal.com>
2024-06-06 15:58:42 -0500

    Condicional Ternario

---
+++ Clase-Ejecucion-Condicional-Ternario.js
@@ -0,0 +1,21 @@
+/*
+let mensaje
+
+const edad = 17;
+
+if (edad >= 18){
+    mensaje = 'Es mayor de Edad'
+}else {
+    mensaje = 'ES menor de Edad'
+}
+
+console.log(mensaje);  */
+
+// condicion ? true: false
+
+const edad = 20 ; 
+const mensaje = 
+edad >= 18 
+? 'Es mayor de edad, puede pasar' 
+: ' Es menor de edad, no puede pasar'
+console.log(mensaje);
