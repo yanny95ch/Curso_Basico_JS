e4ba03add2d34877f9eb741a40f6e7d922e5a9f6
yannych1995 <yannymaly@gmal.com>
2024-06-06 14:22:08 -0500

    Estructuras Condicionales if

---
+++ Clase-Estructuras-Condicionales.js
@@ -0,0 +1,22 @@
+// Estructuras de Control
+
+/* 
+
+if(let===algo) {
+    codigo
+}else if {
+    codigo
+}else {
+
+}
+
+*/
+
+let nombre = 'Nico'
+    if (nombre === 'Diego'){
+        console.log('Hola Diego');
+    }else if(nombre === 'Nico'){
+        console.log('Hola Nico');
+    }else {
+        console.log('Nombre no encontrado');
+    }
