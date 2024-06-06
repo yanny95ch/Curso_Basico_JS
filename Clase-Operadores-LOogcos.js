bbc4c857a69fd73b16be8286e379d0601de606c3
yannych1995 <yannymaly@gmal.com>
2024-06-06 10:11:34 -0500

    Operadores Logicos

---
+++ Clase-Operadores-LOogcos.js
@@ -0,0 +1,16 @@
+/* Operadore LOgicos */ 
+
+// &&  Si un valor  y  otro valor son ... (Y)
+// || si un valor o si otro valor ...  (O)
+// ! (No = Negacion)
+
+const a = 10;
+const b = 20;
+const c = '10'
+
+ console.log( a == b  && a == c );
+  
+ console.log(a != b || a ===c);
+
+ console.log(!(a===c));
+ 
