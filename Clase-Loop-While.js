14b45d905a232cc26eb4c5fb139418dfcf828d53
yannych1995 <yannymaly@gmal.com>
2024-06-08 15:47:49 -0500

    Ciclo While

---
+++ Clase-Loop-While.js
@@ -0,0 +1,14 @@
+/*
+Tambien para poder iterar
+
+while (condicion){
+    Codigo a ejecutar    
+}
+*/
+
+let contador = 0;
+
+while (contador < 10){
+    console.log(contador);
+    contador++ ;
+}
