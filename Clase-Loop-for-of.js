5588b17224e07907f9b73ed4fea28b8d51a6d279
yannych1995 <yannymaly@gmal.com>
2024-06-08 10:38:21 -0500

    Ciclo For Of

---
+++ Clase-Loop-for-of.js
@@ -0,0 +1,12 @@
+/*
+fot of : en objetos iterables  (lista de algo) arrays,  strings
+
+for (variable of  objeto) {
+    codigo a ejecutar
+}
+*/
+
+let canasta = ['Manzana', 'Pera', 'Naranja', 'Uva', 'Mango'];
+for (fruta of canasta){
+    console.log(fruta);
+}
