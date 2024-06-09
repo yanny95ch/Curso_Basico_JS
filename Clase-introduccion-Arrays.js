6437a20cfdd46495f319001882a086139c80a75f
yannych1995 <yannymaly@gmal.com>
2024-06-09 16:59:56 -0500

    Introduccion Arrays Parte2

--- Clase-introduccion-Arrays.js
+++ Clase-introduccion-Arrays.js
@@ -45,3 +45,41 @@ console.log(primeraFruta);
 
 const numberoffrutas = frutas.length;
 console.log(numberoffrutas);
+
+/*Mutabulity
+Si los array son objetos ese objeto puede tener doferentes metodos, que nos van a permitir mutar o transformar el array o crear uno nuevo */
+
+frutas.push('watermelon');
+console.log(frutas);
+
+number.push(9,8);
+console.log(number);
+
+//Immutability
+
+const newfrutas = frutas.concat(['uva','kiwi'])
+console.log(frutas);
+console.log(newfrutas);
+
+const newnumber = number.concat([100,58,23]);
+console.log(number);
+console.log(newnumber);
+
+//Cheking arrays with Array.isArray()
+
+const isArray = Array.isArray(number);
+console.log(isArray);
+
+const isArray2 = Array.isArray(frutas);
+console.log(isArray2);
+
+
+//Practical exercise: sum all elements of an array
+
+    const numbersArray = [1,2,3,4,5]
+    let suma=0
+    let n = numbersArray.length
+    for (let i=0 ; i < n ; i++){
+        suma += numbersArray[i]
+    }
+    console.log(suma);
